084b272667925118444b3cd3e0aac5942a2c420e
iamjothees <iamjothees@gmail.com>
2025-01-01 13:13:27 +0530

    Improv: closeBanners

--- src/app/Filament/User/Pages/Dashboard.php
+++ src/app/Filament/User/Pages/Dashboard.php
@@ -7,4 +7,8 @@ use Filament\Pages\Dashboard as BaseDashboard;
 class Dashboard extends BaseDashboard
 {
     protected static string $view = 'filament.user.pages.dashboard';
+
+    public function closeBanner(string $bannerId): void{
+        session()->put('closed_banners', [...session()->get('closed_banners', []), $bannerId]);
+    }
 }
--- src/resources/views/filament/user/pages/dashboard.blade.php
+++ src/resources/views/filament/user/pages/dashboard.blade.php
@@ -1,12 +1,23 @@
 <x-filament-panels::page>
-    <div class="bg-gradient-to-r from-[#004d00] via-[#005c00] to-[#006600] text-white p-6 rounded-lg shadow-lg max-w-full sm:max-w-3xl mx-auto mt-5">
-        <h1 class="text-2xl sm:text-3xl md:text-4xl font-bold text-center">
-          Refer projects, earn points, and <span class="underline">cash out your share</span>!
-        </h1>
-        <p class="mt-4 text-base sm:text-lg text-center">
-          It's simple, fun, and rewarding. Start sharing today!
-        </p>
+  <div x-data="{ closedBanners: @json(session('closed_banners', []))}" >
+    {{-- Motto Banner --}}
+    <div x-show="!closedBanners.includes($el.id)" id="motto" class="relative bg-gradient-to-r from-[#004d00] via-[#005c00] to-[#006600] text-white p-6 rounded-lg shadow-lg max-w-full mx-auto mt-5">
+      <button
+        wire:click="closeBanner('motto')"
+        x-on:click="$el.parentElement.classList.add('hidden')"
+        class="absolute top-2 right-2 text-white text-xl hover:text-gray-300 transition"
+       >
+        &times;
+      </button>
+      <h1 class="text-2xl sm:text-3xl md:text-4xl font-bold text-center">
+        Refer projects, earn points, and <span class="underline">cash out your share</span>!
+      </h1>
+      <p class="mt-4 text-base sm:text-lg text-center">
+        It's simple, fun, and rewarding. Start sharing today!
+      </p>
     </div>
+  </div>
+  
       
 
     <livewire:users.referal-partner-share-info-card />
