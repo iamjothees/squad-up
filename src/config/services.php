dd9bbb61c437c4eb47fbc7ef0f0476a4e41b9e24
iamjothees <iamjothees@gmail.com>
2025-01-10 22:26:20 +0530

    pause:

--- src/config/services.php
+++ src/config/services.php
@@ -34,5 +34,9 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
-
+    'whatsapp' => [
+        'access_token' => env('WHATSAPP_ACCESS_TOKEN'),
+        'phone_number_id' => env('WHATSAPP_PHONE_NUMBER_ID'),
+        'business_account_id' => env('WHATSAPP_BUSINESS_ACCOUNT_ID'),
+    ],
 ];
